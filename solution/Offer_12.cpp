64fe0e49c7a10e95ef6b3ef2a97324f134f873f0
theRunCom <49315766+theRunCom@users.noreply.github.com>
2023-01-10 15:52:32 +0800

    矩阵中的路径

---
+++ solution/Offer_12.cpp
@@ -0,0 +1,27 @@
+// 原题链接：https://leetcode.cn/problems/ju-zhen-zhong-de-lu-jing-lcof/description/?favorite=xb9nqhhg
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        for (int i = 0; i < board.size(); i++)
+            for (int j = 0; j < board[i].size(); j++)
+                if (dfs(board, word, 0, i, j))
+                    return true;
+        return false;
+    }
+
+    bool dfs(vector<vector<char>>& board, string& word, int u, int x, int y) {
+        if (board[x][y] != word[u]) return false;
+        if (u == word.size() - 1) return true;
+        int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+        char t = board[x][y];
+        board[x][y] = '*';
+        for (int i = 0; i < 4; i++) {
+            int a = x + dx[i], b = y + dy[i];
+            if (a >= 0 && a < board.size() && b >= 0 && b < board[a].size()) {
+                if (dfs(board, word, u + 1, a, b)) return true;
+            }
+        }
+        board[x][y] = t;
+        return false;
+    }
+};
