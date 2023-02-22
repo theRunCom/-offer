5b98739a55d9d9f7fe10b11d4f27c28d97c6f5b9
theRunCom <1772414249@qq.com>
2023-02-22 14:00:43 +0800

    调整数组顺序使奇数位于偶数前面

---
+++ solutions/Offer_21.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> exchange(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+         while (l < r) {
+             while (l < r && nums[l] % 2 == 1) l ++ ;
+             while (l < r && nums[r] % 2 == 0) r -- ;
+             if (l < r) swap(nums[l], nums[r]);
+        }
+        return nums;
+    }
+};
