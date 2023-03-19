34fe31b5a6a3f307b4c8431698d1d209b84b8232
theRunCom <49315766+theRunCom@users.noreply.github.com>
2023-03-19 13:07:10 +0800

    最小的k个数

---
+++ solutions/offer_40.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getLeastNumbers(vector<int>& arr, int k) {
+        priority_queue<int> pq;
+        for (auto x : arr) {
+            pq.push(x);
+            if (pq.size() > k) pq.pop();
+        }
+        vector<int> ans;
+        while (pq.size()) ans.push_back(pq.top()), pq.pop();
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
