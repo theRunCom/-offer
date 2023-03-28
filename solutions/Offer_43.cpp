b891b9f07a10e6e828058709691d78075f4182bd
theRunCom <1772414249@qq.com>
2023-03-28 11:49:08 +0800

    1～n 整数中 1 出现的次数

---
+++ solutions/Offer_43.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countDigitOne(int n) {
+        if (!n) return 0;
+        vector<int> number;
+        while (n) number.push_back(n % 10), n /= 10;
+        long long res = 0;
+        for (int i = number.size() - 1; i >= 0; i -- ) {
+            int left = 0, right = 0, t = 1;
+            for (int j = number.size() - 1; j > i; j -- ) left = left * 10 + number[j];
+            for (int j = i - 1; j >= 0; j -- ) right = right * 10 + number[j], t *= 10;
+            res += left * t;
+            if (number[i] == 1) res += right + 1;
+            else if (number[i] > 1) res += t;
+        }
+        return res;
+    }
+};
