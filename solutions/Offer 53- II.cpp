d55fcc4150b13f7d0f9802ba6832de92c30e69cb
theRunCom <1772414249@qq.com>
2023-03-29 20:31:56 +0800

    0～n-1中缺失的数字

---
+++ solutions/Offer 53- II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res = -1;
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid == nums[mid]) left = mid + 1;
+            else {
+                res = mid;
+                right = mid - 1;
+            }
+        }
+        return res == -1 ? nums.size() : res;
+    }
+};
